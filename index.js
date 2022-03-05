cae6cf6b2b05ccc8f3c0f850cb24d1a55e99162f
andrei <andrei.popa@technologi.co.uk>
2022-03-05 16:58:36 +0000

    fix for front back conn

--- index.js
+++ index.js
@@ -6,15 +6,8 @@ const usersRouter = require('./src/routes/users.route');
 const cors = require('cors')
 
 app.use(cors());
-/*
 app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: false,
-  })
-);
-*/
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
